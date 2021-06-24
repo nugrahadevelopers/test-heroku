bc5b9380aa71669c553afece4f2bda16135604cd
nugrahadevelopers <nug.dev.work@gmail.com>
2021-06-24 13:37:08 +0700

    update 2

--- app/route/customer.route.js
+++ app/route/customer.route.js
@@ -1,5 +1,5 @@
 module.exports = function(app) { 
-    const customers = require('../controller/customer.controller.js');
+    const customers = require('../controller/custumer.controller');
 
     // Retrieve all Customer
     app.get('/api/customers', customers.findAll);
